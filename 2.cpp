d7f535923abec5d63b25e2a1eab15f4e03dec3b8
NurbolotTrueBuddha <86740145+NurbolotTrueBuddha@users.noreply.github.com>
2021-07-01 20:32:05 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+void copyMas(int* source, int* dest, int size)
+{
+	for (int n = 0; n < size; n++) dest[n] = source[n];
+}
+
+
+bool compareMas(int* source, int* dest, int size) 
+{  
+	for (int n = 0; n < size; n++) {
+		if (source[n] != dest[n]) return false;
+		else return true;
+	}
+}
+
+
+
+void test(int* source, int* dest, int size) 
+{ 
+	cout << "\ntest:\n\n";
+	if (compareMas(source, dest, size)) cout << "sucsess\n\n";
+	else cout << "fail\n\n";
+}
+
+int main()
+{
+	const int size = 10;
+
+	int source[size];
+	int dest[size];
+
+	for (int x = 0; x < size; x++) source[x] = rand() % 100;
+
+	copyMas(source, dest, size);
+
+	test(source, dest, size);
+
+}
